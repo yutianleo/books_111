3dedbe66777121d2d829a7e11cd1a154496cc3bb
leo <836283615@qq.com>
2018-12-20 15:42:43 +0800

    解决了冲突

--- books_111/urls.py
+++ books_111/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('book.urls')),
+    url(r'^administrator/', include('book.urls')),
     url(r'^index/', include('book.urls')),
 ]
